81bb800696116c0f60dfd3d91b3dc68b81b0df1b
casey paul <casey.paul@attlasschool.com>
2024-10-09 23:22:59 +0000

    bug fix

--- proc_filesystem/read_write_heap.py
+++ proc_filesystem/read_write_heap.py
@@ -34,16 +34,12 @@ else:
     write_string = ""
 # open the maps and mem files of the process
 maps_filename = "/proc/{}/maps".format(pid)
-print("[*] maps: {}".format(maps_filename))
 mem_filename = "/proc/{}/mem".format(pid)
-print("[*] mem: {}".format(mem_filename))
 
 # try opening the maps file
 try:
     maps_file = open('/proc/{}/maps'.format(pid), 'r')
 except IOError as e:
-    print("[ERROR] Can not open file {}:".format(maps_filename))
-    print("        I/O error({}): {}".format(e.errno, e.strerror))
     sys.exit(1)
 
 for line in maps_file:
@@ -56,38 +52,24 @@ for line in maps_file:
     # parse line
     addr = sline[0]
     perm = sline[1]
-    offset = sline[2]
-    device = sline[3]
-    inode = sline[4]
-    pathname = sline[-1][:-1]
-    print("\tpathname = {}".format(pathname))
-    print("\taddresses = {}".format(addr))
-    print("\tpermisions = {}".format(perm))
-    print("\toffset = {}".format(offset))
-    print("\tinode = {}".format(inode))
 
     # check if there is read and write permission
     if perm[0] != 'r' or perm[1] != 'w':
-        print("[*] {} does not have read/write permission".format(pathname))
         maps_file.close()
         exit(0)
 
     # get start and end of the heap in the virtual memory
     addr = addr.split("-")
     if len(addr) != 2: # never trust anyone, not even your OS :)
-        print("[*] Wrong addr format")
         maps_file.close()
         exit(1)
     addr_start = int(addr[0], 16)
     addr_end = int(addr[1], 16)
-    print("\tAddr start [{:x}] | end [{:x}]".format(addr_start, addr_end))
 
     # open and read mem
     try:
         mem_file = open(mem_filename, 'rb+')
     except IOError as e:
-        print("[ERROR] Can not open file {}:".format(mem_filename))
-        print("        I/O error({}): {}".format(e.errno, e.strerror))
         maps_file.close()
         exit(1)
 
@@ -99,20 +81,21 @@ for line in maps_file:
     try:
         i = heap.index(bytes(search_string, "ASCII"))
     except Exception:
-        print("Can't find '{}'".format(search_string))
         maps_file.close()
         mem_file.close()
         exit(0)
-    print("[*] Found '{}' at {:x}".format(search_string, i))
 
     # write the new string
-    print("[*] Writing '{}' at {:x}".format(write_string, addr_start + i))
     mem_file.seek(addr_start + i)
     mem_file.write(bytes(write_string, "ASCII"))
 
     # close files
     maps_file.close()
     mem_file.close()
+
+    # output only "SUCCESS!" as expected
+
+    print("SUCCESS!") 
 
     # there is only one heap in our example
     break
