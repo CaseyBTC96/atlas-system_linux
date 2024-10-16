23b6cb7522bec33acb5c51687312e536eea92e3c
casey Paul <casey.paul@atlasschool.com>
2024-10-16 23:06:31 +0000

    added 3 more sh_types

--- readelf/1-readelf.c
+++ readelf/1-readelf.c
@@ -31,31 +31,10 @@ int main(int ac, char **av)
 void print_section_32(FILE *file)
 {
 	Elf32_Ehdr header;
+	Elf32_Shdr str_table;
 	Elf32_Shdr section;
-
-	fread(&header, sizeof(header), 1, file);
-	fseek(file, (unsigned int)header.e_shoff, SEEK_SET);
-	fread(&section, sizeof(section), 1, file);
-
-	printf("TEST name = %u\n", (unsigned int)section.sh_name);
-	printf("TEST type = %u\n", (unsigned int)section.sh_type);
-	printf("TEST flags = %u\n", (unsigned int)section.sh_flags);
-	printf("TEST addr = %u\n", (unsigned int)section.sh_addr);
-	printf("TEST offset = %u\n", (unsigned int)section.sh_offset);
-	printf("TEST size = %u\n", (unsigned int)section.sh_size);
-	printf("TEST link = %u\n", (unsigned int)section.sh_link);
-	printf("TEST info = %u\n", (unsigned int)section.sh_info);
-	printf("TEST addralign = %u\n", (unsigned int)section.sh_addralign);
-	printf("TEST entsize = %u\n", (unsigned int)section.sh_entsize);
-}
-
-void print_section_64(FILE *file)
-{
-	Elf64_Ehdr header;
-	Elf64_Shdr str_table;
-	Elf64_Shdr section;
-	char *names = NULL, f_left, f_right, *cur = NULL;
-	int i;
+	char *names = NULL, flags[3];
+	int i, j;
 
 	fread(&header, sizeof(header), 1, file);
 	fseek(file, (unsigned int)header.e_shoff + sizeof(section) * (unsigned int)header.e_shstrndx, SEEK_SET);
@@ -65,7 +44,109 @@ void print_section_64(FILE *file)
 	fread(names, str_table.sh_size, 1, file);
 	fseek(file, header.e_shoff, SEEK_SET);
 
-	printf("There are %d section headers, starting at offset 0x%04x:\n", header.e_shnum, (unsigned int)header.e_shoff);
+	printf("There are %d section headers, starting at offset 0x%x:\n", header.e_shnum, (unsigned int)header.e_shoff);
+	printf("\nSection Headers:\n");
+	printf("  [Nr] Name              Type            Addr     Off    Size   ES Flg Lk Inf Al\n");
+	for (i = 0; i < header.e_shnum; i++)
+	{
+		fread(&section, sizeof(section), 1, file);
+		printf("  [%2d] ", i);
+		printf("%-17s ", names + section.sh_name);
+		if (section.sh_type == SHT_NULL)
+			printf("NULL            ");
+		else if (section.sh_type == SHT_PROGBITS)
+			printf("PROGBITS        ");
+		else if (section.sh_type == SHT_SYMTAB)
+			printf("SYMTAB          ");
+		else if (section.sh_type == SHT_STRTAB)
+			printf("STRTAB          ");
+		else if (section.sh_type == SHT_RELA)
+			printf("RELA            ");
+		else if (section.sh_type == SHT_HASH)
+			printf("HASH            ");
+		else if (section.sh_type == SHT_DYNAMIC)
+			printf("DYNAMIC         ");
+		else if (section.sh_type == SHT_NOTE)
+			printf("NOTE            ");
+		else if (section.sh_type == SHT_NOBITS)
+			printf("NOBITS          ");
+		else if (section.sh_type == SHT_REL)
+			printf("REL             ");
+		else if (section.sh_type == SHT_SHLIB)
+			printf("SHLIB           ");
+		else if (section.sh_type == SHT_DYNSYM)
+			printf("DYNSYM          ");
+		else if (section.sh_type == SHT_GNU_HASH)
+			printf("GNU_HASH        ");
+		else if (section.sh_type == SHT_GNU_versym)
+			printf("VERSYM          ");
+		else if (section.sh_type == SHT_GNU_verneed)
+			printf("VERNEED         ");
+		else if (section.sh_type == SHT_INIT_ARRAY)
+			printf("INIT_ARRAY      ");
+		else if (section.sh_type == SHT_FINI_ARRAY)
+			printf("FINI_ARRAY      ");
+		else if (section.sh_type == SHT_GNU_verdef)
+			printf("VERDEF          ");
+		else if (section.sh_type == LOOS_1)
+			printf("LOOS+ffffff1    ");
+		else if (section.sh_type == LOOS_3)
+			printf("LOOS+ffffff3    ");
+		else
+			printf("UNKNOWN         ");
+
+		printf("%08x ", (unsigned int)section.sh_addr);
+		printf("%06x ", (unsigned int)section.sh_offset);
+		printf("%06x ", (unsigned int)section.sh_size);
+		printf("%02x ", (unsigned int)section.sh_entsize);
+		for (j = 0; j < 3; j++)
+			flags[j] = ' ';
+		j = 2;
+		if (section.sh_flags & SHF_EXECINSTR)
+			flags[j--] = 'X';
+		if (section.sh_flags & SHF_INFO_LINK)
+			flags[j--] = 'I';
+		if (section.sh_flags & SHF_STRINGS)
+			flags[j--] = 'S';
+		if (section.sh_flags & SHF_MERGE)
+			flags[j--] = 'M';
+		if (section.sh_flags & SHF_ALLOC)
+			flags[j--] = 'A';
+		if (section.sh_flags & SHF_WRITE)
+			flags[j--] = 'W';
+		if (section.sh_flags & SHF_EXCLUDE)
+			flags[j--] = 'E';
+		printf("%c%c%c ", flags[0], flags[1], flags[2]);
+		printf("%2u ", (unsigned int)section.sh_link);
+		printf("%3u ", (unsigned int)section.sh_info);
+		printf("%2u\n", (unsigned int)section.sh_addralign);
+	}
+
+printf("Key to Flags:\n");
+printf("  W (write), A (alloc), X (execute), M (merge), S (strings)\n");
+printf("  I (info), L (link order), G (group), T (TLS), E (exclude), x (unknown)\n");
+printf("  O (extra OS processing required) o (OS specific), p (processor specific)\n");
+
+	free(names);
+}
+
+void print_section_64(FILE *file)
+{
+	Elf64_Ehdr header;
+	Elf64_Shdr str_table;
+	Elf64_Shdr section;
+	char *names = NULL, flags[3];
+	int i, j;
+
+	fread(&header, sizeof(header), 1, file);
+	fseek(file, (unsigned int)header.e_shoff + sizeof(section) * (unsigned int)header.e_shstrndx, SEEK_SET);
+	fread(&str_table, sizeof(str_table), 1, file);
+	fseek(file, (unsigned int)str_table.sh_offset, SEEK_SET);
+	names = malloc(sizeof(char) * str_table.sh_size); /* malloc error unhandled */
+	fread(names, str_table.sh_size, 1, file);
+	fseek(file, header.e_shoff, SEEK_SET);
+
+	printf("There are %d section headers, starting at offset 0x%x:\n", header.e_shnum, (unsigned int)header.e_shoff);
 	printf("\nSection Headers:\n");
 	printf("  [Nr] Name              Type            Address          Off    Size   ES Flg Lk Inf Al\n");
 	for (i = 0; i < header.e_shnum; i++)
@@ -107,6 +188,12 @@ void print_section_64(FILE *file)
 			printf("INIT_ARRAY      ");
 		else if (section.sh_type == SHT_FINI_ARRAY)
 			printf("FINI_ARRAY      ");
+		else if (section.sh_type == SHT_GNU_verdef)
+			printf("VERDEF          ");
+		else if (section.sh_type == LOOS_1)
+			printf("LOOS+ffffff1    ");
+		else if (section.sh_type == LOOS_3)
+			printf("LOOS+ffffff3    ");
 		else
 			printf("UNKNOWN         ");
 
@@ -114,35 +201,24 @@ void print_section_64(FILE *file)
 		printf("%06lx ", (unsigned long int)section.sh_offset);
 		printf("%06x ", (unsigned int)section.sh_size);
 		printf("%02x ", (unsigned int)section.sh_entsize);
-		cur = &f_right;
-		f_left = ' ';
-		f_right = ' ';
-		if (section.sh_flags & SHF_ALLOC)
-		{
-			*cur = 'A';
-			cur = &f_left;
-		}
-		if (section.sh_flags & SHF_WRITE)
-		{
-			*cur = 'W';
-			cur = &f_left;
-		}
+		for (j = 0; j < 3; j++)
+			flags[j] = ' ';
+		j = 2;
 		if (section.sh_flags & SHF_EXECINSTR)
-		{
-			*cur = 'X';
-			cur = &f_left;
-		}
-		if (section.sh_flags & SHF_MERGE)
-		{
-			*cur = 'M';
-			cur = &f_left;
-		}
+			flags[j--] = 'X';
+		if (section.sh_flags & SHF_INFO_LINK)
+			flags[j--] = 'I';
 		if (section.sh_flags & SHF_STRINGS)
-		{
-			*cur = 'S';
-			cur = &f_left;
-		}
-		printf("%c%c ", f_left, f_right);
+			flags[j--] = 'S';
+		if (section.sh_flags & SHF_MERGE)
+			flags[j--] = 'M';
+		if (section.sh_flags & SHF_ALLOC)
+			flags[j--] = 'A';
+		if (section.sh_flags & SHF_WRITE)
+			flags[j--] = 'W';
+		if (section.sh_flags & SHF_EXCLUDE)
+			flags[j--] = 'E';
+		printf("%c%c%c ", flags[0], flags[1], flags[2]);
 		printf("%2u ", (unsigned int)section.sh_link);
 		printf("%3u ", (unsigned int)section.sh_info);
 		printf("%2u\n", (unsigned int)section.sh_addralign);
