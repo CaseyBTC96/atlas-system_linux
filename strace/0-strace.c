1950a3b7b30c51bc9a171c9a71a3124c9a637a99
casey paul <casey.paul@atlasschool.com>
2025-01-10 20:21:22 +0000

    fix  bugs in task 0

--- strace/0-strace.c
+++ strace/0-strace.c
@@ -19,22 +19,24 @@ int main(int argc, char *argv[], char *envp[])
 		return (1);
 	}
 
+	setbuf(stdout, NULL);
 	pid = fork();
 
 	if (pid == 0)
 	{
+		printf("59\n"); /* execve syscall number */
 		ptrace(PTRACE_TRACEME, pid, NULL, NULL);
 		execve(argv[1], argv + 1, envp);
 	}
 	else
 	{
-		for (status = 1, print = 1; !WIFEXITED(status); print ^= 1)
+		for (status = 1, print = 0; !WIFEXITED(status); print ^= 1)
 		{
 			ptrace(PT_SYSCALL, pid, NULL, NULL);
 			wait(&status);
 			ptrace(PT_GETREGS, pid, NULL, &regs);
 			if (print)
-				printf("%lu\n", (unsigned long int)regs.orig_rax);
+				printf("%lu\n", (size_t)regs.orig_rax);
 		}
 	}
 
