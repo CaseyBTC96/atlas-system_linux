5da8ce6733414775c58d60cb6c7247ff91d27d8f
casey paul <casey.paul@atlasschool.com>
2025-02-26 03:02:23 +0000

    added code explanation

--- sockets/0-server.c
+++ sockets/0-server.c
@@ -29,29 +29,29 @@ int main(void)
 		perror("Socket creation failed");
 		exit(EXIT_FAILURE);
 	}
-	
+	/* Prepare the sockaddr_in structure */
 	memset(&server_addr, 0, sizeof(server_addr));
-	server_addr.sin_family = AF_INET; 
-	server_addr.sin_addr.s_addr = INADDR_ANY;  
-	server_addr.sin_port = htons(PORT);  
-	
+	server_addr.sin_family = AF_INET; /* IPv4 optimizes direction used*/
+	server_addr.sin_addr.s_addr = INADDR_ANY;  /* Any address */
+	server_addr.sin_port = htons(PORT);  /* Port number*/
+	/* Bind the socket */
 	if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
 	{
 		perror("Bind failed");
 		close(sockfd);
 		exit(EXIT_FAILURE);
 	}
-	if (listen(sockfd, 5) < 0) 
+	if (listen(sockfd, 5) < 0) /* Listen on the socket */
 	{
 		perror("Listen failed");
 		close(sockfd);
 		exit(EXIT_FAILURE);
 	}
 	printf("Server listening on port %d\n", PORT);
-	while (1) 
+	while (1) /* Hang indefinitely */
 	{
-		pause();  
+		pause();  /* Suspend the process until a signal is received */
 	}
-	close(sockfd);
+	close(sockfd); /* Close the socket (not reachable) */
 	return (0);
 }
